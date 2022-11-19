21c8fe417f45d9ce27418d9f5d6589350b98c7c7
David256 <wasterdavid@gmail.com>
2022-11-18 19:46:02 -0500

    Add an script that will called when an event occurs

---
+++ script.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# Take in mind that $1 will be the `GIT_PATH` value.
+
+echo "Edit this script"
+echo
+echo "Example: cd MyProject && pm2 reload ecosystem.config.js"
+echo "target path: $1"
+echo
